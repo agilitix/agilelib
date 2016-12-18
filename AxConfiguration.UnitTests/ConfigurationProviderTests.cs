763a830f631bc145925300e41f35082b151229fd
agilitix <adsbox@outlook.fr>
2016-12-18 17:43:01 +0100

    Added config provider unit test

---
+++ AxConfiguration.UnitTests/ConfigurationProviderTests.cs
@@ -0,0 +1,30 @@
+﻿using AxConfiguration.Interfaces;
+using AxQuality;
+using NUnit.Framework;
+
+namespace AxConfiguration.UnitTests
+{
+    public class ConfigurationProviderTests : ArrangeActAssert
+    {
+        protected IConfigurationProvider ConfigurationProviderUnderTest;
+        protected string ActualKeyValue;
+        protected string ExpectedKey;
+
+        public override void Arrange()
+        {
+            ExpectedKey = "keyValue";
+            ConfigurationProviderUnderTest = new ConfigurationProvider(@".\MainConfiguration");
+        }
+
+        public override void Act()
+        {
+            ActualKeyValue = ConfigurationProviderUnderTest.Configuration.AppSettings.Settings["keyName"].Value;
+        }
+
+        [Test]
+        public void Assert_expected_keyvalue()
+        {
+            Assert.AreEqual(ActualKeyValue, ExpectedKey);
+        }
+    }
+}
