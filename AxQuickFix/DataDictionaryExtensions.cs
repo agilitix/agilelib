0156ce360102df852ee3200a5f2c6765dc9d7b23
agilitix <adsbox@outlook.fr>
2017-03-04 16:04:22 +0100

    Added QuickFixN extensions, upgraded some nugets

---
+++ AxQuickFix/DataDictionaryExtensions.cs
@@ -0,0 +1,110 @@
+﻿
+using System.Collections.Generic;
+using QuickFix.DataDictionary;
+
+namespace AxQuickFix
+{
+    public static class DataDictionaryExtensions
+    {
+        public static string GetFieldName(this DataDictionary self, int tag)
+        {
+            string fieldName;
+            TryGetFieldName(self, tag, out fieldName);
+            return fieldName;
+        }
+
+        public static bool TryGetFieldName(this DataDictionary self, int tag, out string fieldName)
+        {
+            DDField ddField;
+            if (self.FieldsByTag.TryGetValue(tag, out ddField))
+            {
+                fieldName = ddField.Name;
+                return true;
+            }
+
+            fieldName = string.Empty;
+            return false;
+        }
+
+        public static int GetFieldTag(this DataDictionary self, string fieldName)
+        {
+            int fieldTag;
+            TryGetFieldTag(self, fieldName, out fieldTag);
+            return fieldTag;
+        }
+
+        public static bool TryGetFieldTag(this DataDictionary self, string fieldName, out int tag)
+        {
+            DDField ddField;
+            if (self.FieldsByName.TryGetValue(fieldName, out ddField))
+            {
+                tag = ddField.Tag;
+                return true;
+            }
+
+            tag = 0;
+            return false;
+        }
+
+        public static string GetEnumLabel(this DataDictionary self, int tag, string enumValue)
+        {
+            string enumLabel;
+            TryGetEnumLabel(self, tag, enumValue, out enumLabel);
+            return enumLabel;
+        }
+
+        public static bool TryGetEnumLabel(this DataDictionary self, int tag, string enumValue, out string enumLabel)
+        {
+            enumLabel = string.Empty;
+            DDField ddField;
+            return self.FieldsByTag.TryGetValue(tag, out ddField)
+                   && ddField.EnumDict.TryGetValue(enumValue, out enumLabel);
+        }
+
+        public static string GetEnumValue(this DataDictionary self, int tag, string enumLabel)
+        {
+            string enumValue;
+            TryGetEnumValue(self, tag, enumLabel, out enumValue);
+            return enumValue;
+        }
+
+        public static bool TryGetEnumValue(this DataDictionary self, int tag, string enumLabel, out string enumValue)
+        {
+            DDField ddField;
+            if (self.FieldsByTag.TryGetValue(tag, out ddField))
+            {
+                foreach (KeyValuePair<string, string> enumDictInfo in ddField.EnumDict)
+                {
+                    if (enumDictInfo.Value.Equals(enumLabel))
+                    {
+                        enumValue = enumDictInfo.Key;
+                        return true;
+                    }
+                }
+            }
+
+            enumValue = string.Empty;
+            return false;
+        }
+
+        public static string GetFieldFixType(this DataDictionary self, int tag)
+        {
+            string fixFieldType;
+            TryGetFieldFixType(self, tag, out fixFieldType);
+            return fixFieldType;
+        }
+
+        public static bool TryGetFieldFixType(this DataDictionary self, int tag, out string fixFieldType)
+        {
+            DDField ddField;
+            if (self.FieldsByTag.TryGetValue(tag, out ddField))
+            {
+                fixFieldType = ddField.FixFldType;
+                return true;
+            }
+
+            fixFieldType = string.Empty;
+            return false;
+        }
+    }
+}
---
+++ AxQuickFix/MessageExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+using QuickFix;
+using QuickFix.DataDictionary;
+using QuickFix.Fields;
+
+namespace AxQuickFix
+{
+    public static class MessageExtensions
+    {
+        public static string GetName(this Message self, DataDictionary dictionary)
+        {
+            string msgType = self.Header.GetField(Tags.MsgType);
+            string msgName = dictionary.GetEnumLabel(Tags.MsgType, msgType);
+            return msgName;
+        }
+
+        public static XDocument ToXDocument(this Message self, DataDictionary dictionary)
+        {
+            string msgName = GetName(self, dictionary);
+            XDocument doc = new XDocument(new XDeclaration("1.0", "UTF-8", null));
+
+            XElement root = new XElement("message");
+            root.Add(new XAttribute("name", msgName));
+            doc.Add(root);
+
+            XElement header = new XElement("header");
+            root.Add(header);
+            foreach (KeyValuePair<int, IField> field in self.Header)
+            {
+                XElement fieldElement = CreateElement(field, dictionary);
+                header.Add(fieldElement);
+            }
+
+            XElement body = new XElement("body");
+            root.Add(body);
+            foreach (KeyValuePair<int, IField> field in self)
+            {
+                XElement fieldElement = CreateElement(field, dictionary);
+                body.Add(fieldElement);
+            }
+
+            XElement trailer = new XElement("trailer");
+            root.Add(trailer);
+            foreach (KeyValuePair<int, IField> field in self.Trailer)
+            {
+                XElement fieldElement = CreateElement(field, dictionary);
+                trailer.Add(fieldElement);
+            }
+
+            return doc;
+        }
+
+        private static XElement CreateElement(KeyValuePair<int, IField> field, DataDictionary dictionary)
+        {
+            string fieldName = dictionary.GetFieldName(field.Key);
+            string fieldValue = field.Value.ToString();
+            string fieldFixType = dictionary.GetFieldFixType(field.Key);
+
+            XElement element = new XElement(fieldName);
+
+            element.Add(new XAttribute("tag", field.Key));
+
+            if (fieldFixType.Equals("DATA"))
+            {
+                try
+                {
+                    XElement data = XElement.Parse(fieldValue);
+                    element.Add(data);
+                }
+                catch
+                {
+                    element.Add(new XCData(fieldValue));
+                }
+            }
+            else
+            {
+                element.Add(new XAttribute("value", fieldValue));
+                string name = dictionary.GetEnumLabel(field.Key, fieldValue);
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    element.Add(new XAttribute("desc", name));
+                }
+            }
+
+            element.Add(new XAttribute("type", fieldFixType));
+
+            return element;
+        }
+    }
+}
