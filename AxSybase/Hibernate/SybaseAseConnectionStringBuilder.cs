23dacff694f7d4b399ef7c4636dd6e5f990868f6
agilitix <adsbox@outlook.fr>
2017-03-25 08:31:43 +0100

    Added comments

--- AxSybase/Hibernate/SybaseAseConnectionStringBuilder.cs
+++ AxSybase/Hibernate/SybaseAseConnectionStringBuilder.cs
@@ -5,6 +5,15 @@ namespace AxSybase.Hibernate
 {
     public class SybaseAseConnectionStringBuilder : ConnectionStringBuilder
     {
+        // Please look there for several connection string samples.
+        // Feel free to customise this class with new parameters.
+        // https://www.connectionstrings.com/ase-ado-net-data-provider/
+        //
+        // Look there for the charset value. You must setup the
+        // charset like in the ASE server if you got an exception
+        // "Unsupported Charset" at OpenSession.
+        // https://www.connectionstrings.com/ase-unsupported-charset/
+
         private string _server;
         private int _port;
         private string _database;
@@ -56,27 +65,27 @@ namespace AxSybase.Hibernate
 
         protected override string Create()
         {
-            string str = base.Create();
-            if (!string.IsNullOrEmpty(str))
+            string connectionString = base.Create();
+            if (!string.IsNullOrEmpty(connectionString))
             {
-                return str;
+                return connectionString;
             }
 
-            StringBuilder connectionString = new StringBuilder();
+            StringBuilder connectionStringBuilder = new StringBuilder();
 
-            connectionString.AppendFormat("Data Source={0};Port={1};Database={2};Uid={3};Pwd={4}",
-                                          _server,
-                                          _port,
-                                          _database,
-                                          _username,
-                                          _password);
+            connectionStringBuilder.AppendFormat("Data Source={0};Port={1};Database={2};Uid={3};Pwd={4}",
+                                                 _server,
+                                                 _port,
+                                                 _database,
+                                                 _username,
+                                                 _password);
 
             if (!string.IsNullOrWhiteSpace(_charset))
             {
-                connectionString.AppendFormat(";Charset={0}", _charset);
+                connectionStringBuilder.AppendFormat(";Charset={0}", _charset);
             }
 
-            return connectionString.ToString();
+            return connectionStringBuilder.ToString();
         }
     }
 }
