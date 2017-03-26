f462e4d4e8bebb49572d2c80da85f149e5047123
agilitix <adsbox@outlook.fr>
2017-03-27 00:43:11 +0200

    Added Dapper helper

---
+++ AxSybase/Dapper/AseCommandParameters.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Sybase.Data.AseClient;
+
+namespace AxSybase.Dapper
+{
+    public class AseCommandParameters : List<AseParameter>, SqlMapper.IDynamicParameters
+    {
+        public void AddParameters(IDbCommand command, SqlMapper.Identity identity)
+        {
+            foreach (AseParameter parameter in this)
+            {
+                command.Parameters.Add(parameter);
+            }
+        }
+    }
+}
