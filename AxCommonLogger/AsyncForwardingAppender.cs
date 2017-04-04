ff52603e7d5b567f56ccf3b255ef450e040fa9eb
agilitix <adsbox@outlook.fr>
2017-04-04 21:26:41 +0200

    Changed log4net async appender base class

---
+++ AxCommonLogger/AsyncForwardingAppender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using AxUtils;
+using AxUtils.Interfaces;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Util;
+
+namespace AxCommonLogger
+{
+    public class AsyncForwardingAppender : ForwardingAppender
+    {
+        protected IWorkerQueue<Action> _workerQueue;
+
+        public AsyncForwardingAppender()
+        {
+            _workerQueue = new WorkerQueue<Action>(appendLog => appendLog());
+            _workerQueue.OnWorkerQueueException += (s, a) =>
+                                                   {
+                                                       LogLog.Error(typeof(AsyncForwardingAppender),
+                                                                    "WorkerQueue has raised an exception in the log4net async appender",
+                                                                    a.Exception);
+                                                   };
+        }
+
+        [DebuggerStepThrough]
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (!_workerQueue.TryAdd(() => base.Append(loggingEvent)))
+            {
+                base.Append(loggingEvent);
+            }
+        }
+
+        [DebuggerStepThrough]
+        protected override void Append(LoggingEvent[] loggingEvents)
+        {
+            if (!_workerQueue.TryAdd(() => base.Append(loggingEvents)))
+            {
+                base.Append(loggingEvents);
+            }
+        }
+
+        [DebuggerStepThrough]
+        protected override void OnClose()
+        {
+            _workerQueue.Cancel();
+            base.OnClose();
+        }
+    }
+}
