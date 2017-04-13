0fe0f9e5419c1ab88ac23f8257858ca6a1566966
agilitix <adsbox@outlook.fr>
2017-04-13 22:44:19 +0200

    Added log4net reactive appender

---
+++ AxCommonLogger/Appenders/Log4netReactiveForwardingAppender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using log4net.Appender;
+using log4net.Core;
+
+namespace AxCommonLogger.Appenders
+{
+    public class Log4netReactiveForwardingAppender : ForwardingAppender
+    {
+        private readonly EventLoopScheduler _orderedScheduler = new EventLoopScheduler();
+        protected ISubject<LoggingEvent[]> _loggingSubject = new Subject<LoggingEvent[]>();
+
+        public Log4netReactiveForwardingAppender()
+        {
+            _loggingSubject.AsObservable()
+                           .ObserveOn(_orderedScheduler)
+                           .Subscribe(x => base.Append(x));
+        }
+
+        [DebuggerStepThrough]
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            _loggingSubject.OnNext(new[] {loggingEvent});
+        }
+
+        [DebuggerStepThrough]
+        protected override void Append(LoggingEvent[] loggingEvents)
+        {
+            _loggingSubject.OnNext(loggingEvents);
+        }
+
+        [DebuggerStepThrough]
+        protected override void OnClose()
+        {
+            _loggingSubject.OnCompleted();
+            base.OnClose();
+        }
+    }
+}
