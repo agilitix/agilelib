78f66296fd0364de6f4f6337fe401eb5fbfb6aa7
agilitix <adsbox@outlook.fr>
2017-04-15 09:42:52 +0200

    Added little wpf sample to explore ReactiveUI

---
+++ AxWpfReactiveUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace AxWpfReactiveUI.ViewModels
+{
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public ReactiveCommand Delete { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            Delete = ReactiveCommand.CreateFromObservable(Something);
+        }
+
+        private IObservable<Unit> Something()
+        {
+
+            return Observable.Return(Unit.Default);
+        }
+    }
+}
---
+++ AxWpfReactiveUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using AxWpfReactiveUI.ViewModels;
+using ReactiveUI;
+
+namespace AxWpfReactiveUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ViewModel = AppDependencies.MainWindowViewModel;
+
+            this.WhenAnyValue(x => x.ViewModel).BindTo(this, x => x.DataContext);
+            this.BindCommand(ViewModel, vm => vm.Delete, v => v.MainButton);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MainWindowViewModel) value; }
+        }
+
+        public MainWindowViewModel ViewModel { get; set; }
+    }
+}
