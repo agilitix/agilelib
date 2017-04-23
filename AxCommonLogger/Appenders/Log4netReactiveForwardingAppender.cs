60459159257e987cf9f02b72c025f1838231129f
agilitix <adsbox@outlook.fr>
2017-04-23 20:10:38 +0200

    Added some comments on reactive scheduler

--- AxCommonLogger/Appenders/Log4netReactiveForwardingAppender.cs
+++ AxCommonLogger/Appenders/Log4netReactiveForwardingAppender.cs
@@ -11,16 +11,18 @@ namespace AxCommonLogger.Appenders
 {
     public class Log4netReactiveForwardingAppender : ForwardingAppender, IDisposable
     {
+        // The EventLoopScheduler preserve order of events on dedicated thread.
+        protected EventLoopScheduler _scheduler = new EventLoopScheduler();
+
+        protected ISubject<LoggingEvent> _loggingSubject = new Subject<LoggingEvent>();
         protected IDisposable _cleanUp;
-        protected EventLoopScheduler _orderedScheduler = new EventLoopScheduler();
-        protected ISubject<LoggingEvent[]> _loggingSubject = new Subject<LoggingEvent[]>();
 
         public Log4netReactiveForwardingAppender()
         {
             IDisposable appender = _loggingSubject.AsObservable()
-                                                  .ObserveOn(_orderedScheduler)
+                                                  .ObserveOn(_scheduler)
                                                   .Where(x => x != null)
-                                                  .Subscribe(x => base.Append(x));
+                                                  .Subscribe(base.Append);
 
             _cleanUp = Disposable.Create(() => { appender.Dispose(); });
         }
@@ -28,13 +30,16 @@ namespace AxCommonLogger.Appenders
         [DebuggerStepThrough]
         protected override void Append(LoggingEvent loggingEvent)
         {
-            _loggingSubject.OnNext(new[] {loggingEvent});
+            _loggingSubject.OnNext(loggingEvent);
         }
 
         [DebuggerStepThrough]
         protected override void Append(LoggingEvent[] loggingEvents)
         {
-            _loggingSubject.OnNext(loggingEvents);
+            foreach (LoggingEvent loggingEvent in loggingEvents)
+            {
+                _loggingSubject.OnNext(loggingEvent);
+            }
         }
 
         [DebuggerStepThrough]
@@ -52,8 +57,8 @@ namespace AxCommonLogger.Appenders
             _loggingSubject?.OnCompleted();
             _loggingSubject = null;
 
-            _orderedScheduler?.Dispose();
-            _orderedScheduler = null;
+            _scheduler?.Dispose();
+            _scheduler = null;
         }
     }
 }
