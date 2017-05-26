66975f2c24ac8054028b2c552beaf914a1b1ecac
agilitix <adsbox@outlook.fr>
2017-05-26 11:05:46 +0200

    Added helpers for culture setup and data-annotations validator

---
+++ AxUtils/CultureInfoUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace AxUtils
+{
+    public static class CultureInfoUtils
+    {
+        public static void SetDefaultCultureInfo_en_US()
+        {
+            var culture = new CultureInfo("en-US");
+            SetDefaultCultureInfo(culture);
+        }
+
+        public static void SetDefaultCultureInfo(CultureInfo culture)
+        {
+            // Should work for all frameworks.
+            Thread.CurrentThread.CurrentCulture.GetType().GetProperty("DefaultThreadCurrentCulture")?.SetValue(Thread.CurrentThread.CurrentCulture, culture, null);
+            Thread.CurrentThread.CurrentCulture.GetType().GetProperty("DefaultThreadCurrentUICulture")?.SetValue(Thread.CurrentThread.CurrentCulture, culture, null);
+        }
+    }
+}
---
+++ AxUtils/ModelValidatorExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace AxUtils
+{
+    /*
+       Validate properties :
+
+            public class MyClass
+            {
+                [Required]
+                [StringLength(10, MinimumLength = 3)]
+                public string Name { get; set; }
+            }
+
+            class Program
+            {
+                MyClass obj = new MyClass();
+                obj.Name = "Jo";
+
+                List<ValidationResult> results;
+                bool isValid = obj.IsValid(out results); // "The field Name must be a string with a minimum length of 3 and a maximum length of 10."
+            }
+     */
+
+    public class InvalidModelException : Exception
+    {
+        public ValidationResult[] ValidationResults { get; }
+
+        public InvalidModelException(IEnumerable<ValidationResult> results)
+        {
+            ValidationResults = results.ToArray();
+        }
+    }
+
+    public static class ModelValidatorExtensions
+    {
+        public static void Validate(this object self)
+        {
+            List<ValidationResult> results;
+            bool isValid = IsValid(self, out results);
+            if (!isValid)
+            {
+                throw new InvalidModelException(results);
+            }
+        }
+
+        public static bool IsValid(this object self)
+        {
+            var validationContext = new ValidationContext(self, null, null);
+            return Validator.TryValidateObject(self, validationContext, null, true);
+        }
+
+        public static bool IsValid(this object self, out List<ValidationResult> results)
+        {
+            var validationContext = new ValidationContext(self, null, null);
+            results = new List<ValidationResult>();
+            return Validator.TryValidateObject(self, validationContext, results, true);
+        }
+    }
+}
