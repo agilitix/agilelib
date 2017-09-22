b2e5d979940774416c01f17f0abcd3c98a5e5409
agilitix <adsbox@outlook.fr>
2017-09-22 17:53:31 +0200

    Updated projects to vs2017

--- AxRepository/Repository.cs
+++ AxRepository/Repository.cs
@@ -1,72 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using AxRepository.Interfaces;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Text;
+//using AxRepository.Interfaces;
 
-namespace AxRepository
-{
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
-    {
-        protected readonly DbContext _context;
-        protected readonly DbSet _entities;
+//namespace AxRepository
+//{
+//    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+//    {
+//        protected readonly DbContext _context;
+//        protected readonly DbSet _entities;
 
-        public Repository(DbContext context)
-        {
-            _context = context;
-            _entities = _context.Set<TEntity>();
-        }
+//        public Repository(DbContext context)
+//        {
+//            _context = context;
+//            _entities = _context.Set<TEntity>();
+//        }
 
-        public TEntity Get(int id)
-        {
-            return _context.Set<TEntity>().Find(id);
-        }
+//        public TEntity Get(int id)
+//        {
+//            return _context.Set<TEntity>().Find(id);
+//        }
 
-        public IEnumerable<TEntity> GetAll()
-        {
-            // Note that here I've repeated _context.Set<TEntity>() in every method and this is causing
-            // too much noise. I could get a reference to the DbSet returned from this method in the 
-            // constructor and store it in a private field like _entities. This way, the implementation
-            // of our methods would be cleaner:
-            // 
-            // _entities.ToList();
-            // _entities.Where();
-            // _entities.SingleOrDefault();
-            // 
-            // I didn't change it because I wanted the code to look like the videos. But feel free to change
-            // this on your own.
-            return _context.Set<TEntity>().ToList();
-        }
+//        public IEnumerable<TEntity> GetAll()
+//        {
+//            // Note that here I've repeated _context.Set<TEntity>() in every method and this is causing
+//            // too much noise. I could get a reference to the DbSet returned from this method in the 
+//            // constructor and store it in a private field like _entities. This way, the implementation
+//            // of our methods would be cleaner:
+//            // 
+//            // _entities.ToList();
+//            // _entities.Where();
+//            // _entities.SingleOrDefault();
+//            // 
+//            // I didn't change it because I wanted the code to look like the videos. But feel free to change
+//            // this on your own.
+//            return _context.Set<TEntity>().ToList();
+//        }
 
-        public IEnumerable<TEntity> Find(Func<TEntity, bool> predicate)
-        {
-            return _context.Set<TEntity>().Where(predicate);
-        }
+//        public IEnumerable<TEntity> Find(Func<TEntity, bool> predicate)
+//        {
+//            return _context.Set<TEntity>().Where(predicate);
+//        }
 
-        public TEntity SingleOrDefault(Func<TEntity, bool> predicate)
-        {
-            return _context.Set<TEntity>().SingleOrDefault(predicate);
-        }
+//        public TEntity SingleOrDefault(Func<TEntity, bool> predicate)
+//        {
+//            return _context.Set<TEntity>().SingleOrDefault(predicate);
+//        }
 
-        public void Add(TEntity entity)
-        {
-            _context.Set<TEntity>().Add(entity);
-        }
+//        public void Add(TEntity entity)
+//        {
+//            _context.Set<TEntity>().Add(entity);
+//        }
 
-        public void AddRange(IEnumerable<TEntity> entities)
-        {
-            _context.Set<TEntity>().AddRange(entities);
-        }
+//        public void AddRange(IEnumerable<TEntity> entities)
+//        {
+//            _context.Set<TEntity>().AddRange(entities);
+//        }
 
-        public void Remove(TEntity entity)
-        {
-            _context.Set<TEntity>().Remove(entity);
-        }
+//        public void Remove(TEntity entity)
+//        {
+//            _context.Set<TEntity>().Remove(entity);
+//        }
 
-        public void RemoveRange(IEnumerable<TEntity> entities)
-        {
-            _context.Set<TEntity>().RemoveRange(entities);
-        }
-    }
-}
+//        public void RemoveRange(IEnumerable<TEntity> entities)
+//        {
+//            _context.Set<TEntity>().RemoveRange(entities);
+//        }
+//    }
+//}
---
+++ AxUtils/DateTimeISO8601.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AxUtils
+{
+    public static class DateTimeISO8601
+    {
+        private static readonly int _lenUnsKind = "2017-09-19T15:16:11.6008521".Length;
+        private static readonly int _lenUtcKind = "2017-09-19T13:15:48.8446795Z".Length;
+        private static readonly int _lenLocKind = "2017-09-19T15:16:11.6008521+02:00".Length;
+
+        public static string ToString(DateTime input)
+        {
+            return input.ToString("o");
+        }
+
+        public static string ToString(DateTimeOffset input)
+        {
+            return input.ToString("o");
+        }
+
+        public static DateTime ParseDateTime(string input)
+        {
+            return ToDateTime(input);
+        }
+
+        public static DateTimeOffset ParseDateTimeOffset(string input)
+        {
+            return ToDateTimeOffset(input);
+        }
+
+        private static DateTimeOffset ToDateTimeOffset(string dateTime)
+        {
+            DateTimeOffset dto = DateTimeOffset.Parse(dateTime);
+            return dto;
+        }
+
+        private static DateTime ToDateTime(string dateTime)
+        {
+            DateTimeOffset dto = DateTimeOffset.Parse(dateTime);
+            if (dateTime.Length == _lenLocKind) // Local kind
+                return dto.LocalDateTime;
+            if (dateTime.Length == _lenUtcKind) // Utc kind
+                return dto.UtcDateTime;
+            if (dateTime.Length == _lenUnsKind) // Unspecified kind
+                return dto.DateTime;
+
+            throw new InvalidOperationException("Invalid date time string");
+        }
+    }
+}
