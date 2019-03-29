1f05d059914a5ed99603d186df5cda4267900aaf
agilitix <adsbox@outlook.fr>
2019-03-29 22:50:18 +0100

    Code refactoring on string hasher

---
+++ AxCrypt.UnitTests/StringHasherTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace AxCrypt.UnitTests
+{
+    [TestFixture]
+    internal class StringHasherTest
+    {
+        [Test]
+        public void Given_text_Should_be_hash_to_md5()
+        {
+            /*
+             * Arrange.
+             */
+            string text = "the text";
+            string md5 = "9d54f4c0c77def4be7880a85991b04c5";
+
+            /*
+             * Act.
+             */
+            StringHasher hasher = new StringHasher(MD5.Create);
+            string hash = hasher.ComputeHash(text);
+
+            /*
+             * Assert.
+             */
+            hash.Should().Be(md5);
+        }
+    }
+}
---
+++ AxCrypt/StringHasher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AxCrypt
+{
+    public class StringHasher
+    {
+        private readonly Func<HashAlgorithm> _hasher;
+
+        public StringHasher(Func<HashAlgorithm> hasher)
+        {
+            _hasher = hasher;
+        }
+
+        public string ComputeHash(string input)
+        {
+            using (HashAlgorithm hash = _hasher())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] inputHashBytes = hash.ComputeHash(inputBytes);
+
+                var sb = new StringBuilder();
+                for (int index = 0; index < inputHashBytes.Length; ++index)
+                {
+                    byte b = inputHashBytes[index];
+                    sb.Append(b.ToString("x2")); // Hexadecimal.
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public bool VerifyHash(string input, string hash)
+        {
+            string inputHash = ComputeHash(input);
+            return inputHash == hash;
+        }
+    }
+}
